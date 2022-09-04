3d1d3fd5554860d0965f7db46319fc5331824762
Christopher Guerrero <chrisgrr3@gmail.com>
2022-09-04 10:50:44 -0700

    Struct and State Variable Construction

---
+++ Ballot1/contracts/Ballot.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.17;
+
+contract Ballot {
+
+    struct Voter {
+        uint weight;
+        bool voted; // Keeps track of whether or not a voter voted
+        uint8 vote; // Keeps track of which proposal the voter voted for.
+    }
+
+    //modifier
+    modifier onlyOwner() {
+        require(msg.sender == chairperson);
+        _;
+    }
+
+    address public chairperson;
+    mapping(address => Voter) public voters;
+    uint[4] public proposals;
+}
